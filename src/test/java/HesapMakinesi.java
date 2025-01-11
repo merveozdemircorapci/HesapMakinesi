bd7e4e091e855c768909531dd7999b13dcd6cb86
merveozdemircorapci <mrve.ozdemir@gmail.com>
2025-01-11 20:17:47 +0300

    Merve Özdemir Çorapçı - Terfi Uygulama Otomasyon Sınavı

---
+++ src/test/java/HesapMakinesi.java
@@ -0,0 +1,69 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class HesapMakinesi {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://catchylabs-webclient.testinium.com/"); //bu adrese git
+        driver.manage().window().maximize();
+
+        WebElement Username = driver.findElement(By.xpath("//input[@placeholder='Username']"));   //elamanın id bilgisi yazılacak. Tarayıcıda ilgili anlanın sağ tik incele yapıldıüındaki İd bilgisi
+        Username.click(); // kutucuğuan tıkla.
+        Username.sendKeys("merve.ozdemir");  //senkeys alana girelecek değer bilgisi
+
+        WebElement Password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        Password.click();
+        Password.sendKeys("Ozdemir3!");
+
+        WebElement Login = driver.findElement(By.xpath("//div[@class='css-146c3p1 r-jwli3a r-1b43r93']"));   //css ile yazdırma
+        Login.click();
+        Thread.sleep(1000);
+
+        WebElement OpenCalculator = driver.findElement(By.xpath("//div[@class='css-175oi2r r-1i6wzkk r-lrvibr r-1loqt21 r-1otgn73 r-1awozwy r-169ebfh r-z2wwpe r-h3s6tt r-1777fci r-tsynxw r-11c0sde r-13qz1uu']"));   //css ile yazdırma
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", (OpenCalculator));
+
+        WebElement bir = driver.findElement(By.xpath("//div[contains(text(),'1')]"));
+        WebElement sekiz = driver.findElement(By.xpath("//div[contains(text(),'8')]"));
+        WebElement iki = driver.findElement(By.xpath("//div[14]"));
+        WebElement esittir = driver.findElement(By.cssSelector("div:nth-child(20) div:nth-child(1) div:nth-child(1)"));
+        WebElement sifir = driver.findElement(By.xpath("//div[17]"));
+        WebElement cikarma = driver.findElement(By.cssSelector("div:nth-child(12)"));
+
+        bir.click();
+        sifir.click();
+        sifir.click();
+        sifir.click();
+        cikarma.click();
+        sekiz.click();
+        sifir.click();
+        sifir.click();
+        esittir.click();
+
+        Thread.sleep (1000);
+
+        WebElement sonuc1 = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div[1]/div/div/div/div[1]/div/span"));
+
+        System.out.println("Sonuç Alanı Bulundu.");
+        String sonuc1text = sonuc1.getText(); //elementi aldım 1 sayısını buldum.
+        System.out.println("Beklenen Sonuç: 200, Gerçekleşen Sonuç: " + sonuc1text);
+        try {
+            Assert.assertEquals(sonuc1text, "= 200","Sonuç hatalıdır");
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+        finally {
+            driver.quit();
+        }
+    }
+}
